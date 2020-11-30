d17719112136250f13ac0aeeb179553eaf03d2ac
linzi <50197267+shishezhang@users.noreply.github.com>
2020-11-30 11:40:39 +0800

    Create 113002实现接口App.java

---
+++ 113002实现接口App.java
@@ -0,0 +1,12 @@
+public class MainDemo implements R{
+
+    public static void main(String[] args) {
+        System.out.println(boy_filePath);
+        System.out.println(girl_filePath);
+        for (int[] i: arr) {
+            for (int j:i) {
+                System.out.println(j);
+            }
+        }
+    }
+}
