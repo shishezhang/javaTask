88ea8fb94de740bc837ad2594996243172dd3f2c
linzi <50197267+shishezhang@users.noreply.github.com>
2020-11-30 11:39:37 +0800

    Create 113002接口R.java

---
+++ 113002接口R.java
@@ -0,0 +1,6 @@
+public interface R {
+    static String boy_filePath = "img/boy.gif";
+    static String girl_filePath = "img/girl.gif";
+    static int[][] arr = new int[][]{{1, 2, 3}, {4, 5, 6}};
+}
+
