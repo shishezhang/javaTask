fcd8f01e712570265c40d0906234ec2f42848dd4
linzi <50197267+shishezhang@users.noreply.github.com>
2020-12-02 23:04:26 +0800

    Update 120201.方向键控制图片移动.App.java

--- 120201.方向键控制图片移动.App.java
+++ 120201.方向键控制图片移动.App.java
@@ -1,3 +1,10 @@
+package Demo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class DemoMain {
      JPanel mypanel = new JPanel();
      JLabel aircraft = new JLabel();
@@ -26,18 +33,20 @@ public class DemoMain {
                 super.keyPressed(e);
                 int x = aircraft.getX();
                 int y = aircraft.getY();
+                int H = (int) jFrame.getSize().getHeight()-64;
+                int W = (int) jFrame.getSize().getWidth()-64;
                 if(e.getKeyCode() == KeyEvent.VK_LEFT){
                     System.out.println("触发左");
-                    x-=5;
+                    x = x<=0 ? 0 : x-10;
                 }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
                     System.out.println("触发右");
-                    x+=5;
+                    x = x>=W ? W : x+10;
                 }else if(e.getKeyCode() == KeyEvent.VK_UP){
                     System.out.println("触发上");
-                   y-=5;
+                   y = y<=0 ? 0 : y-15;
                 }else if(e.getKeyCode() == KeyEvent.VK_DOWN){
                     System.out.println("触发下");
-                    y+=5;
+                    y = y>=H ? H : y+15;
                 }
                 aircraft.setLocation(x,y);
             }
